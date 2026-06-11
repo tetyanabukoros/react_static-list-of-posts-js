7426015aa00957084bd4d340cbae349b42c0cf95
tetbuk <tetiana.bukoros@dreamhost.com>
2026-06-12 01:25:42 +0300

    removed extra signs

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -21,7 +21,6 @@ export const PostInfo = ({ post }) => {
         <CommentList comments={post.comments} />
       ) : (
         <>
-          {' '}
           <hr />
           <b data-cy="NoCommentsMessage">No comments yet</b>
         </>
